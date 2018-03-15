7acd9a2f72bff16026a0dcc91a95eb1f961536e7
AndyLithia <thelithcore@gmail.com>
2018-03-15 23:02:52 +0800

    ROM

--- ROM.v
+++ ROM.v
@@ -30,12 +30,14 @@ module QUAD_ROM(
 	input				sync,
 
 	input				ia,
-	output				is,
+	output wire			is,
 	output				ws,
 	output wire [1:0]	d_page
 );
 
 reg [5:0]		sys_cnt_r;
+
+reg [7:0]		ia_buf_sr;
 
 reg [7:0]		adr_buf_r;
 reg [2:0]		ws_buf_r;
@@ -46,10 +48,19 @@ wire [9:0]		rom_read_adr;
 wire [9:0]		rom_read_data;
 
 wire			te_t11;
+wire			te_t28;
+wire			te_t29;
 wire			te_t55;
+wire			te_is;
+
 
 assign te_t11 = sys_cnt_r == 6'd11;
+assign te_t28 = sys_cnt_r == 6'd28;					// Latch Address(?)
+assign te_t29 = sys_cnt_r == 6'd29;					// Latch Data
 assign te_t55 = sys_cnt_r == 6'd55;
+assign te_is = (sys_cnt_r>=6'd45)&&(sys_cnt_r<=6'd54);
+
+assign is = te_t11 || (inst_buf_r[0]&&te_is);
 
 always @ (posedge cph1 or negedge sync) begin
 	if(~sync)					sys_cnt_r <= 0;
@@ -62,11 +73,25 @@ assign rom_read_adr = {rom_page_id, adr_buf_r};
 
 always @ (posedge cph1 or posedge pon) begin
 	if(pon)	rom_page_id <= 0; // Power On Seq
-	else 
+	else begin 
+		if(te_t55&&(inst_buf_r[6:0]==7'b0_0100_00)
+			rom_page_id <= inst_buf_r[9:7];
+
+		if(te_is)	inst_buf_r <= {inst_buf_r[0], inst_buf_r[9:1]};
+	end
 end
 
+// Lattice EBR ROM primitive (init'd by HP35ROM.mem)
+ROM10b ROMinst(
+	.Address(rom_read_adr),
+	.OutClock(te_t28),
+	.OutClockEn(1'b1),
+	.Reset(1'b0),
+	.Q(rom_read_data)
+);
 
-
-
+always @ (posedge te_t29) begin
+	inst_buf_r <= rom_read_data;
+end
 
 endmodule // QUAD_ROM
